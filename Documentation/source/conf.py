8a248a44f8989c0694d17dea783a26e4ce5ce309
Alix ANNERAUD <alix.anneraud@outlook.fr>
2021-03-31 00:09:29 +0200

    Update documentation conf.py

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -33,9 +33,8 @@ import subprocess, os
 
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
-if read_the_docs_build:
 
-    subprocess.call('cd ../Doxygen; doxygen', shell=True)
+subprocess.call('cd ../Doxygen; doxygen', shell=True)
 
 
 extensions = [
