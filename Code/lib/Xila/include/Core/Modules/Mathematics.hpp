0ea6b010a299d9345459b03e97af0e6849e904ca
Alix ANNERAUD <alix.anneraud@outlook.fr>
2021-07-06 00:55:49 +0200

    Implement mathematics features

---
+++ Code/lib/Xila/include/Core/Modules/Mathematics.hpp
@@ -0,0 +1,235 @@
+#include "Arduino.h"
+
+#ifndef MATHEMATICS_HPP_INCLUDED
+#define MATHEMATICS_HPP_INCLUDED
+
+inline bool Xila_Class::Mathematics_Class::Is_NAN(double Number)
+{
+    return isnan(Number);
+}
+
+inline bool Xila_Class::Mathematics_Class::Is_Infinite(double Number)
+{
+    return isinf(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Copy_Sign(double Number_1, double Number_2)
+{
+    return copysign(Number_1, Number_2);
+}
+
+inline double Xila_Class::Mathematics_Class::Floor(double Number)
+{
+    return floor(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Modulo(double Number_1, double Number_2)
+{
+    return fmod(Number_1, Number_2);
+}
+
+inline double Xila_Class::Mathematics_Class::Radians(double Degrees)
+{
+    return Degrees * Degrees_To_Radians;
+}
+
+inline double Xila_Class::Mathematics_Class::Degrees(double Radians)
+{
+    return Radians * Radians_To_Degrees;
+}
+
+inline double Xila_Class::Mathematics_Class::Sine(double Number)
+{
+    return sin(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Cosine(double Number)
+{
+    return cos(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Tangent(double Number)
+{
+    return tan(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Secant(double Number)
+{
+    return 1 / Cosine(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Cosecant(double Number)
+{
+    return 1 / Sine(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Cotangeant(double Number)
+{
+    return 1 / Tangent(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Sine(double Number)
+{
+    return asin(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Cosine(double Number)
+{
+    return acos(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Tangent(double Number)
+{
+    return atan(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Secant(double Number)
+{
+    return Arc_Cosine(1 / Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Cosecant(double Number)
+{
+    return Arc_Sine(1 / Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Cotangent(double Number)
+{
+    return (Pi / 2) - Arc_Tangent(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Hyperbolic_Sine(double Number)
+{
+    return sinh(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Hyperbolic_Cosine(double Number)
+{
+    return cosh(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Hyperbolic_Tangent(double Number)
+{
+    return tanh(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Hyperbolic_Secant(double Number)
+{
+    return 1 / Hyperbolic_Cosine(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Hyperbolic_Cosecant(double Number)
+{
+    if (Number == 0)
+    {
+        return nan("");
+    }
+    return 1 / Hyperbolic_Sine(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Hyperbolic_Cotangent(double Number)
+{
+    if (Number == 0)
+    {
+        return nan("");
+    }
+    return Hyperbolic_Cosine(Number) / Hyperbolic_Sine(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Hyperbolic_Sine(double Number)
+{
+    return asinh(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Hyperbolic_Cosine(double Number)
+{
+    return acosh(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Hyperbolic_Tangent(double Number)
+{
+    return atanh(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Hyperbolic_Secant(double Number)
+{
+    return 1 / Hyperbolic_Secant(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Hyperbolic_Cosecant(double Number)
+{
+    return 1 / Hyperbolic_Cosecant(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Arc_Hyperbolic_Cotangent(double Number)
+{
+    return 1 / Hyperbolic_Cotangent(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Absolute(double Number)
+{
+    return abs(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Power(double Base, double Exponent)
+{
+    return pow(Base, Exponent);
+}
+
+inline double Xila_Class::Mathematics_Class::Square(double Number)
+{
+    return Number * Number;
+}
+
+inline double Xila_Class::Mathematics_Class::Cube(double Number)
+{
+    return Number * Number * Number;
+}
+
+inline double Xila_Class::Mathematics_Class::Root(double Base, double Exponent)
+{
+    return Power(Base, 1 / Exponent);
+}
+
+inline double Xila_Class::Mathematics_Class::Square_Root(double Number)
+{
+    return sqrt(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Cubic_Root(double Number)
+{
+    return cbrt(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Exponential(double Number)
+{
+    return exp(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Logarithm(double Base, double Number)
+{
+    return Natural_Logarithm(Number) / Natural_Logarithm(Base);
+}
+
+inline double Xila_Class::Mathematics_Class::Binary_Logarithm(double Number)
+{
+    return log2(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Natural_Logarithm(double Number)
+{
+    return log(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Decimal_Logarithm(double Number)
+{
+    return log10(Number);
+}
+
+inline double Xila_Class::Mathematics_Class::Inverse(double Number)
+{
+    return 1 / Number;
+}
+
+
+#endif
--- Code/lib/Xila/src/Core/Modules/Mathematics.cpp
+++ Code/lib/Xila/src/Core/Modules/Mathematics.cpp
@@ -1,11 +1,15 @@
 #include "Core/Core.hpp"
 
+double Xila_Class::Mathematics_Class::String_To_Float(const char *String)
+{
+    return atof(String);
+}
+
 char *Xila_Class::Mathematics_Class::Float_To_String(double Number, int8_t Width, uint8_t Precision, char *String)
 {
     dtostrf(Number, Width, Precision, String);
-    uint8_t i;
     // -- Delete unwanted zeros
-    for (i = sizeof(String) - 1; i > 0; i--)
+    for (uint8_t i = sizeof(String) - 1; i > 0; i--)
     {
         if (String[i] == '0' || String[i] == '\0')
         {
@@ -22,3 +26,48 @@ char *Xila_Class::Mathematics_Class::Float_To_String(double Number, int8_t Width
     }
     return String;
 }
+
+double Xila_Class::Mathematics_Class::Greatest_Common_Divisor(double Number_1, double Number_2)
+{
+    if (Number_1 == 0 || Number_2 == 0)
+    {
+        return 0;
+    }
+    else if (Number_1 == Number_2)
+    {
+        return Number_1;
+    }
+    else if (Number_1 > Number_2)
+    {
+        return Greatest_Common_Divisor(Number_1 - Number_2, Number_2);
+    }
+    else
+    {
+        return Greatest_Common_Divisor(Number_1, Number_2 - Number_1);
+    }
+}
+
+double Xila_Class::Mathematics_Class::Least_Common_Multiple(double Number_1, double Number_2)
+{
+    return ((Number_1 * Number_2) / Greatest_Common_Divisor(Number_1, Number_2));
+}
+
+double Xila_Class::Mathematics_Class::Factorial(double Number)
+{
+    for (uint8_t i = Number - 1; i > 0; i--)
+    {
+        Number *= i;
+    }
+    return Number;
+}
+
+double Xila_Class::Mathematics_Class::Combination(double n, double k)
+{
+    return Factorial(n) / (Factorial(n - k) * Factorial(k));
+}
+
+double Xila_Class::Mathematics_Class::Permutation(double n, double k)
+{
+    return Factorial(n) / Factorial(n - k);
+}
+
